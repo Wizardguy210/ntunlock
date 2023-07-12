2c9a424061d94611c7d31a884e0d1ddbf3dfb581
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-12 11:15:55 -0500

    Wizardguy210/ntunlock

--- ntunlock/settings_linux.go
+++ ntunlock/settings_linux.go
@@ -1,14 +1,14 @@
-package ntunlock
+# package ntunlock
 
-import (
-	"os"
-	"fmt"
+# import (
+# 	"os"
+# 	"fmt"
 )
 
-func GetPlatformSettings() *Settings {
-	s := Settings{[]string{}}
-	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/.config/nuclearthrone", os.Getenv("HOME")))
-	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/.steam/steam/%s", os.Getenv("HOME"), SteamAppPath))
-
-	return &s
+# func GetPlatformSettings() *Settings {
+# 	s := Settings{[]string{}}
+# 	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/.config/nuclearthrone", os.Getenv("HOME")))
+# 	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/.steam/steam/%s", os.Getenv("HOME"), SteamAppPath))
+# 
+# 	return &s
 }
